8cc9b24e9531f2a4e9bd0cf3e3ea7b498d362d60
Beatrice Kern <beatrice.kern23@gmail.com>
2020-05-22 08:36:17 +0200

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+#we use open uri to real the url
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+#JSON.parse turns it into ruby object
+ingredients_hash = JSON.parse(ingredients_serialized)
+
+ingredients_hash["drinks"].each do |hash| 
+    Ingredient.create(name: hash["strIngredient1"])
+end
+
+
